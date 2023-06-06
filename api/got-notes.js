60763009c18be14ee5295cdb9375fc880cedf3ff
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2023-06-05 21:11:01 -0700

    adding some boiler plate code for lamda functions

---
+++ api/got-notes.js
@@ -0,0 +1,29 @@
+/**
+ * Route: GET /notes
+ */
+
+import { AWS } from 'aws-sdk';
+import { getResponseHeaders } from './util';
+
+const dynamoDb = new AWS.DynamoDb.DocumentClient();
+const tableName = process.env.NOTES_TABLE;
+
+exports.handler = async (event) => {
+	try {
+		return {
+			statusCode: 200,
+			headers: getResponseHeaders(),
+			body: JSON.stringify(''),
+		};
+	} catch (err) {
+		console.log('Error', err);
+		return {
+			statusCode: err.statusCode ? err.statusCode : 500,
+			headers: getResponseHeaders(),
+			body: JSON.stringify({
+				error: err.name ? err.name : 'Exception',
+				message: err.message ? err.message : 'Unknown error',
+			}),
+		};
+	}
+};
